c6b91565ddf8b830ddc44587abf07ede44bdb7c5
Pragya <42120039+pragyaagupta@users.noreply.github.com>
2018-10-20 20:47:17 +0530

    Create staircase.cpp
    Hacker rank problem.

---
+++ staircase.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int n,i,j,k;
+    cin >> n;
+
+    for(i=n;i>=1;i--)
+   {
+        for(j=1;j<i;j++)
+        {
+            cout<<" ";
+        }
+        for(k=n;k>=i;k--)
+        {
+            cout<<"#";
+        }
+        cout<<endl;
+    }    
+
+    return 0;
+}
